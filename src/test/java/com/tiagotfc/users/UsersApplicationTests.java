85354523ff12ce290a0b037704e31c9b1c0725b3
tiagoc0sta <tiagodpv@gmail.com>
2024-10-10 14:11:31 -0400

    initial

---
+++ src/test/java/com/tiagotfc/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiagotfc.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
